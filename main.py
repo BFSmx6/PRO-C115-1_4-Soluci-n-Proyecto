4abc835a6ad9bb949ab4f1c2c24a9cb69c428634
BFSmx6 <97541827+BFSmx6@users.noreply.github.com>
2022-05-05 18:18:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 import os
   
   
-# Source file path
+# Ruta del archivo source - (fuente)
 source = 'main.txt'
   
 # destination file path
 dest = 'newfile.txt'
   
   
-# Now rename the source path
-# to destination path
-# using os.rename() method
+# Ahora renombra la ruta source
+# Ruta de destino
+# Usa el método os.rename() 
 os.rename(source, dest)
-print("Source path renamed to destination path successfully.")
+print("La ruta Source fue renombrada ruta de destino exitosamente.")
